6910eadd120c753146fb3eecae7131966e54700f
Giovanni Silva <git@gsilva.pro>
2024-06-19 22:13:08 -0300

    simulando formulario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { RouterOutlet } from '@angular/router';
+import { Dog } from './models'
 
 @Component({
   selector: 'app-root',
@@ -23,18 +24,25 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'fundatectlpIV-aula-forms';
-
+  dogs: Dog[] = []
   form: FormGroup
 
   constructor(formBuilder: FormBuilder) {
     this.form = formBuilder.group({
-      name: [],
-      race: []
+      name: ['', Validators.required], // array de configurações
+      race: ['', Validators.compose([Validators.required, Validators.minLength(5)])] // compose junta mais de uma validação
     })
   }
 
   save() {
-    console.log(this.form.value)
+    if(this.form.valid) {
+      console.log('formulario valido')
+      this.dogs.push(this.form.value)
+    } else {
+      console.error('formulario invalido')
+    }
+    console.log('Items: ')
+    console.log(this.dogs)
   }
 
 }
