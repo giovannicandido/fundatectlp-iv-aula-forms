4961c00ae8be031658bf5a28ddd3e456250b590f
Giovanni Silva <git@gsilva.pro>
2024-06-25 20:23:21 -0300

    exemplo de select no form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,18 +5,23 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
 import { RouterOutlet } from '@angular/router';
 import { Dog } from './models'
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, 
+  imports: [
+    CommonModule,
+    RouterOutlet, 
     MatFormFieldModule, 
     MatIconModule, 
     MatInputModule,
     MatCardModule,
     MatButtonModule,
+    MatSelectModule,
     ReactiveFormsModule
   ],
   templateUrl: './app.component.html',
@@ -25,12 +30,14 @@ import { Dog } from './models'
 export class AppComponent {
   title = 'fundatectlpIV-aula-forms';
   dogs: Dog[] = []
+  vacines = ['Vacina 1', 'Vacina 2', 'Vacina 3']
   form: FormGroup
 
   constructor(formBuilder: FormBuilder) {
     this.form = formBuilder.group({
-      name: ['', Validators.required], // array de configurações
-      race: ['', Validators.compose([Validators.required, Validators.minLength(5)])] // compose junta mais de uma validação
+      name: ['', Validators.compose([Validators.required, Validators.minLength(10), Validators.maxLength(50)])], // array de configurações
+      race: ['', Validators.compose([Validators.required, Validators.minLength(5)])], // compose junta mais de uma validação
+      vacine: ['']
     })
   }
 
